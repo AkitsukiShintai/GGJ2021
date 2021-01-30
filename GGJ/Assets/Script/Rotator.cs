7eed23fe4a97219dca28becf18458d3948fdf0f5
A-RAVEN <769561232@qq.com>
2021-01-30 20:30:01 +0800

    修改法阵材质初始值，添加旋转脚本

---
+++ GGJ/Assets/Script/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [Header("旋转轴")]
+    public Vector3 rotationAxis = new Vector3(0.0f, 1.0f, 0.0f);
+    [Header("旋转速度")]
+    public float rotationSpeed = 1.0f;
+
+    void Update()
+    {
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime, Space.Self);
+    }
+}
