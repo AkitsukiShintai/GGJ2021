3f74ae79c9b74f67d3cd63dbdb9c14de4154b9b5
Run0812 <traft@126.com>
2021-01-30 12:48:19 +0800

    add monster

---
+++ GGJ/Assets/Script/Monster.cs
@@ -0,0 +1,68 @@
+﻿using Assets.Script;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+
+public enum MonsterAttackType { };
+
+public enum MonsterMoveType { };
+
+
+public class MonsterStatus {
+
+    public float rage;
+    public MonsterAttackType attackType;
+    public MonsterMoveType moveType;
+    public Vector3 moveTarget;
+
+}
+
+
+public class Monster : MonoBehaviour
+{
+
+    public MonsterConfig cfg;
+    private MonsterStatus status;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // public方法设置MonsterStatus, Update检查并更新动画机的状态
+    void Update()
+    {
+
+    }
+
+    public void MoveTo(Vector3 traget, MonsterMoveType type)
+    {
+
+    }
+
+    public void AttackBy(MonsterAttackType type)
+    {
+
+    }
+
+    void EatStar(Star star)
+    {
+        float ragePerStar = 5.0f;
+        status.rage -= ragePerStar;
+    }
+
+    bool CheckDeath()
+    {
+        return status.rage < cfg.deadRage;
+    }
+
+    void Dead()
+    {
+        // 触发一个死亡事件
+        // 处理销毁、死亡动画等
+    }
+}
