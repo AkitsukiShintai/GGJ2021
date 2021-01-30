04def5ed67795b6f17e7617d8e891cc85e69634f
AkitsukiShintai <zxfwzq1995@gmail.com>
2021-01-30 21:43:22 +0800

    复活

--- GGJ/Assets/Script/PlayerMove.cs
+++ GGJ/Assets/Script/PlayerMove.cs
@@ -20,28 +20,86 @@ public class PlayerMove : MonoBehaviour
 
     private float m_currentH = 0;
     private Vector3 m_currentDirection = Vector3.zero;
-
     /// <summary>
     /// 插值
     /// </summary>
     private readonly float m_interpolation = 10;
-
     private bool m_isGrounded;
     private bool m_IsDie;
-
     /// <summary>
     /// 突进
     /// </summary>
     private bool isFast = false;
-
     private bool isHit = false;
     private List<Collider> m_collisions = new List<Collider>();
+    public float jumpForce;
+    private int scale = 1;
+    private int starCounter = 1;
+    private StarTargetManager targetManager;
 
     private void Awake()
     {
-        playerData.jump = 4;
+        targetManager = GetComponent<StarTargetManager>();
+        jumpForce = playerData.jump;
         if (m_animator == null) { m_animator = GetComponent<Animator>(); }
         if (m_rigidBody == null) { m_rigidBody = GetComponent<Rigidbody>(); }
+        Player.eatStarEvents += Player_eatStarEvents;
+        Player.vomitStarEvents += Player_vomitStarEvents;
+    }
+    private void OnDestroy()
+    {
+        Player.eatStarEvents -= Player_eatStarEvents;
+        Player.vomitStarEvents -= Player_vomitStarEvents;
+    }
+    /// <summary>
+    /// 吐星星
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="star"></param>
+    private void Player_vomitStarEvents(Player player, Star star)
+    {
+        if (player != gameObject.GetComponent<Player>()) return;
+        SetScale(-(int)playerData.amplification);
+        GameObject target = null;
+        if (targetManager.m_inRangeEnemys.Count > 0)
+        {
+            int value = UnityEngine.Random.Range(0, 10);
+            if (value < 5)
+                target = targetManager.m_inRangeEnemys[UnityEngine.Random.Range(0, targetManager.m_inRangeEnemys.Count)].gameObject;
+            else
+            {
+                Player otherPlayer = Player.FindAnotherPlayer(gameObject.GetComponent<Player>());
+                target = otherPlayer.gameObject;
+            }
+        }
+        else
+        {
+            Player otherPlayer = Player.FindAnotherPlayer(gameObject.GetComponent<Player>());
+            target = otherPlayer.gameObject;
+        }
+        star.VomitStar(target);
+    }
+    /// <summary>
+    /// 吃星星
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="star"></param>
+    private void Player_eatStarEvents(Player player, Star star)
+    {
+        if (player != gameObject.GetComponent<Player>()) return;
+        SetScale((int)playerData.amplification);
+    }
+    /// <summary>
+    /// 设置人物变大缩小
+    /// </summary>
+    public void SetScale(int value)
+    {
+        scale += value;
+        transform.localScale = Vector3.one * scale;
+        if (value > 0)
+            jumpForce += scale;
+        else
+            jumpForce -= scale + 1;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -102,6 +160,7 @@ public class PlayerMove : MonoBehaviour
     private void FixedUpdate()
     {
         if (m_IsDie) return;
+
         //if (isFast)
         //{
         //    m_animator.SetFloat("MoveSpeed", 1);
@@ -115,40 +174,33 @@ public class PlayerMove : MonoBehaviour
         //    Vector3 pos = transform.position; pos.z = 0; transform.position = pos;
         //    return;
         //}
-        MoveUpdate();
-    }
 
-    private void Update()
-    {
-        if (m_IsDie) return;
-        m_animator.SetBool("Grounded", m_isGrounded);
-        if (Input.GetKeyDown(playerData.jumpKey) && m_isGrounded)
-        {
-            m_rigidBody.AddForce(Vector3.up * playerData.jump, ForceMode.Impulse);
-            m_animator.SetTrigger("Jump");
-        }
-        //if (Input.GetKeyDown(playerData.fastKey)) Fast();
-        //if (Input.GetKeyDown(playerData.attackKey)) NormalAttack();
-        //if (Input.GetKeyDown(playerData.rangeAttackKey)) { RangeAttack(); SetScale(3); }
-    }
-
-    private void MoveUpdate()
-    {
         float h = 0;
         switch (m_PlayerType)
         {
             case PlayerType.Left:
                 h = Input.GetAxis("LeftHorizontal");
                 break;
-
             case PlayerType.Right:
                 h = Input.GetAxis("RightHorizontal");
                 break;
-
             default:
                 break;
         }
-
+        Move(h);
+    }
+    private void Update()
+    {
+        if (m_IsDie) return;
+        m_animator.SetBool("Grounded", m_isGrounded);
+        if (Input.GetKeyDown(playerData.jumpKey) && m_isGrounded)
+        {
+            m_rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            m_animator.SetTrigger("Jump");
+        }
+    }
+    public void Move(float h)
+    {
         m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
 
         Vector3 direction = Vector3.right * m_currentH;
@@ -166,74 +218,38 @@ public class PlayerMove : MonoBehaviour
             m_animator.SetFloat("MoveSpeed", direction.magnitude);
         }
     }
-
     public void Die()
     {
-        gameObject.SetActive(false);
         m_IsDie = true;
         m_animator.SetTrigger("Die");
     }
-
-    /// <summary>
-    /// 设置人物变大缩小
-    /// </summary>
-    public void SetScale(int value)
-    {
-        if (transform.localScale.x == value) return;
-        transform.localScale = Vector3.one * value;
-        playerData.jump *= value;
-    }
-
     /// <summary>
     /// 突进
     /// </summary>
-    public void Fast()
-    {
-        isFast = true;
-        StartCoroutine(Delay(() =>
-        {
-            isFast = false;
-            m_animator.SetFloat("MoveSpeed", 0);
-        }, 0.1f));
-    }
-
-    private void RangeAttack()
-    {
-        //TODO播放攻击动画，生成攻击特效
-    }
-
-    private bool CircleAttack(Transform attacked, Transform skillPosition, float radius)
-    {
-        float distance = Vector3.Distance(attacked.position, skillPosition.position);
-        if (distance < radius)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// 普通攻击
-    /// </summary>
-    public void NormalAttack()
-    {
-    }
+    //public void Fast()
+    //{
+    //    isFast = true;
+    //    StartCoroutine(Delay(() =>
+    //    {
+    //        isFast = false;
+    //        m_animator.SetFloat("MoveSpeed", 0);
+    //    }, 0.1f));
+    //}
 
     /// <summary>
     /// 受击
     /// </summary>
-    public void Hit()
-    {
-        isHit = true;
-        StartCoroutine(Delay(() =>
-        {
-            isHit = false;
-        }, 0.05f));
-    }
-
-    private IEnumerator Delay(Action action, float delaySeconds)
-    {
-        yield return new WaitForSeconds(delaySeconds);
-        action();
-    }
+    //public void Hit()
+    //{
+    //    isHit = true;
+    //    StartCoroutine(Delay(() =>
+    //    {
+    //        isHit = false;
+    //    }, 0.05f));
+    //}
+    //private IEnumerator Delay(Action action, float delaySeconds)
+    //{
+    //    yield return new WaitForSeconds(delaySeconds);
+    //    action();
+    //}
 }
