3947783180a18af941b72f4d0787be172c61027b
A-RAVEN <769561232@qq.com>
2021-01-30 15:40:15 +0800

    添加基础机关

---
+++ GGJ/Assets/Script/Toggler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggler : MonoBehaviour
+{
+    [Header("相应的机关")]
+    public BasicToggle basicToggle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            basicToggle?.OnToggle();
+        }
+    }
+}
